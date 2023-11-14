14a4cef1a201ba41f085767ffd5ef1696dcd2a3c
RMI01 <rmi01@fadi-ims.com>
2023-11-14 19:36:02 +0300

    Initial

---
+++ src/mainpage.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+#Contents displayed on the Homepage
+def mainpage():
+    st.write("We can detect 7 emotions! ")
+    st.write('Angry :angry:, Disgust :persevere:, Fear 😨, Happy :smile:, Sad :worried:, Suprise :astonished:, Neutral :expressionless:.')
+    st.write("Smile to the camera :smile: !")
